352367d0812cfd5cf8c73251fb7b84a853c41bf8
rich17lucas <richard.lucas@caymancomputers.co.uk>
2019-11-08 06:52:44 +0000

    End of Chapter 2

--- Chapter02/02_cartpole_random.py
+++ Chapter02/02_cartpole_random.py
@@ -2,7 +2,6 @@ import gym
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
-
     total_reward = 0.0
     total_steps = 0
     obs = env.reset()
--- Chapter02/04_cartpole_random_monitor.py
+++ Chapter02/04_cartpole_random_monitor.py
@@ -3,13 +3,15 @@ import gym
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
-    env = gym.wrappers.Monitor(env, "recording", force=True)
+    # Record the environment - remove Force=True to prevent overwriting
+    env = gym.wrappers.Monitor(env, "~/tmp/recording", force=True)
 
     total_reward = 0.0
     total_steps = 0.0
     obs = env.reset()
 
     while True:
+        env.render(mode="human")                        # Display the cart
         action = env.action_space.sample()
         obs, reward, done, _ = env.step(action)
         total_reward += reward
